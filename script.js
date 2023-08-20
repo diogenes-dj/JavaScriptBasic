d9258363ea1a33afbc7ae2ffe070609262ce50d0
diogenes-dj <zyguifried@hotmail.com>
2023-08-20 16:58:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Qual é o seu nome?");
+var sobrenome = prompt("Qual é o seu sobrenome?");
+
+console.log("Olá" + nome + " " + sobrenome);
+
+document.write("<h1> Bem vindo " + nome + " </h1>")
+document.write("<img src='https://sujeitoprogramado.com/steve.png' alt='Foto do Steve' />")
